a3fa2c904b008403cac3af18cd86e456e3e8ebd7
Andrey <flytner@users.noreply.github.com>
2018-03-30 09:22:34 +0300

    Support threading.

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 import requests
 import os
-import sys
 import time
+import threading
+from queue import Queue
 
-url = input("Input thread url: ")
-while not (url.endswith(".html") and (url.startswith("https://") or url.startswith("http://"))):
-    url = input("Input correct thread url!!!: ")
-url = url.replace("html", "json")
+start = time.time()
+def downloader():
+    while True:
+        file = queue.get()
+        r = requests.get('https://2ch.hk/b/src/' + thread_id + '/' + file, stream = True)
+        length = r.headers.get('content-length')
+
+        print('{} Downloading: {} ({} KB)'.format(threading.current_thread().name, file, length))
+
+        with open(os.path.join(directory, file), 'wb') as fd:
+            for chunk in r.iter_content(chunk_size=1024):
+                fd.write(chunk)
+
+        queue.task_done()
+
+thread_url = input("Input thread url: ")
+while not (thread_url.endswith(".html") and (thread_url.startswith("https://") or thread_url.startswith("http://"))):
+    thread_url = input("Input correct thread url!!!: ")
+thread_url = thread_url.replace("html", "json")
 
 try:
-    r = requests.get(url)
+    r = requests.get(thread_url)
     r.raise_for_status()
 except requests.RequestException as e:
     exit(e)
 
 d = r.json()
-start = time.time()
-total = 0
-total_kb = 0
-title = d['title']
+all_files = []
+threads = 10
 thread_id = d['current_thread']
+title = d['title']
 directory = title + ' id' + thread_id
 
 print('GET ', title)
@@ -27,32 +42,30 @@ if not os.path.exists(directory):
     print("Make dir: ", directory, "\n")
     os.makedirs(directory)
 else:
-    print("The directory is already exist. Downloading the rest videos...\n")
+    print("The directory is already exist. Downloading the rest videos...")
 
 for i in d['threads'][0]['posts']:
     if i['files']:
         each_post = [x['name'] for x in i['files']]
         for each_file in each_post:
             if 'webm' in each_file or 'mp4' in each_file:
+                all_files.append(each_file)
                 if os.path.exists(os.path.join(directory, each_file)):
                     print(each_file, "already downloaded", end='\r')
                     time.sleep(0.01)
                     continue
-                total += 1
-                dl = 0
-                r = requests.get('https://2ch.hk/b/src/' + thread_id + '/' + each_file, stream=True)
-                length = int(r.headers.get('content-length'))
-                total_kb += length
-                with open(os.path.join(directory, each_file), 'wb') as fd:
-                    for chunk in r.iter_content(chunk_size=1024):
-                        dl += len(chunk)
-                        fd.write(chunk)
-                        done = int(25 * dl / length)
+print("Starting {} threads for {} files".format(threads, len(all_files)))
+queue = Queue()
 
-                        print("%s: %s [%s%s] %s KB / %s KB" % (total, each_file, '=' * done,\
-                        ' ' * (25 - done), int(dl / 1024), int(length / 1024)), end='\r')
+for i in range(threads):
+    t = threading.Thread(target=downloader)
+    t.setDaemon(True)
+    t.start()
 
+for file in all_files:
+    queue.put(file)
 
-print("\nDone, files: ", total)
-print(time.time() - start, "seconds")
-print(total_kb, "KB")
+queue.join()
+print('total {}'.format(len(all_files)))
+end = time.time()
+print(end - start, 'seconds')
